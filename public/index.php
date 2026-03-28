e8d3fb85de298b92efa3b3781c8c53d989602161
sintcov <58187832+sintcov@users.noreply.github.com>
2026-03-28 10:37:58 +0300

    Изменение сообщение комита

--- public/index.php
+++ public/index.php
@@ -1,2 +1,16 @@
-<?php
-    echo 'Привет мир';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        echo 'Hello world';
+
+    ?>
+    
+</body>
+</html>
