2c8173b5a2e41333bf6b66ff81659b8e279ae204
sintcov <58187832+sintcov@users.noreply.github.com>
2026-03-30 09:31:00 +0300

    добавлен заголовок

--- public/index.php
+++ public/index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>GIT</title>
 </head>
 <body>
+<h1>Заголовок</h1>
     <?php
 
         echo 'Hello world';
